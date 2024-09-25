1b46e55c88d023e8d4b19c445d1b3f66e88f83f3
JoseMore99 <57239404+JoseMore99@users.noreply.github.com>
2024-09-24 20:51:50 -0600

    feat(Backend): Reportar problema
    Se creo el endpoint para reportar un problema

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -4,8 +4,26 @@ const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 const Role = require('../models/Role'); // Importa Role correctamente
 const UserRole = require('../models/UserRole'); // Asegúrate de importar UserRole
+const Reporte = require('../models/Reporte')
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const router = express.Router();
+
+function verificarToken(req, res, next) {
+  const token = req.headers['authorization'];
+  if (!token) {
+    return res.status(403).send('Token no proporcionado');
+  }
+  const secretKey =  process.env.JWT_SECRET; 
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).send('Token no válido o expirado');
+    }
+    req.user = decoded;
+    next();
+  });
+}
 
 // Ruta para crear un nuevo usuario
 router.post('/create-user', [
@@ -46,5 +64,52 @@ router.post('/create-user', [
     }
   }
 });
+
+router.post('/reportar-problema',verificarToken, [
+  check('descripcion', 'La descripcion es obligatoria').not().isEmpty(),
+  check('fecha', 'La fecha es obligatoria').isDate()
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { idReportado,nombreReportado, descripcion, fecha, tipo } = req.body;
+
+  try {
+    console.log(req.user.user.correo)
+    const reportante = await User.findOne({ where: { correo: req.user.user.correo } });
+    if (!reportante) {
+      return res.status(500).send('Usuario no encontrado');
+    }
+    var idConductor=idReportado
+    if (idConductor==null){
+      const reportado = await User.findOne({ where:  { nombre_completo: nombreReportado } });
+      if (!reportado) {
+        idConductor=0
+      }else{
+        idConductor=reportado.id
+      }
+    }
+
+    const nuevoReporte = await Reporte.create({
+      id_reportante: reportante.id,
+      id_reporteado: idConductor,
+      descripcion: descripcion,
+      fecha: fecha,
+      tipo: tipo 
+    }); 
+
+    res.status(201).json({ msg: 'Reporte de problema creado exitosamente', nuevoReporte });
+
+  } catch (error) {
+    console.error(error.message);
+    // Envía una respuesta solo si no se ha enviado ya una
+    if (!res.headersSent) {
+      res.status(500).send('Error al reportar un problema');
+    }
+  }
+});
+
+
 
 module.exports = router;
